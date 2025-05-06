f22e63070433a73d97ebdaddf4351a3bba8f67b2
Kengo-Miyauchi <m.ken5.0120@gmail.com>
2025-05-06 17:56:25 +0900

    change attention class

--- tab_network.py
+++ tab_network.py
@@ -1154,7 +1154,7 @@ class TimeSeriesTabNetPretraining(torch.nn.Module):
             virtual_batch_size=virtual_batch_size,
             momentum=momentum,
         )
-        self.self_attn = LightCausalAttention(embed_dim=n_d, num_heads=4,dropout=0)
+        self.self_attn = LightCausalAttention(embed_dim=n_d, dropout=0)
         print("Using Self Attention")
 
 
