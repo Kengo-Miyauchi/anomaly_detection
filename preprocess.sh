f8962009a47ce27b1282da4f5f672db160c35612
Kengo-Miyauchi <m.ken5.0120@gmail.com>
2025-07-09 13:37:41 +0900

    change file locations

--- preprocess.sh
+++ preprocess.sh
@@ -1,4 +1,4 @@
 python preprocess.py \
-  --captions_fpath /mnt/iot-qnap5/miyauchi/data/paired_txt.csv \
-  --scada_dpath    /mnt/iot-qnap5/miyauchi/data/paired_data \
-  --encoder_model_path /mnt/iot-qnap5/miyauchi/model/haenkaze/tabnet-pretrain-out2023-40dim
+  --captions_fpath /../data/paired_txt.csv \
+  --scada_dpath    /../data/paired_data \
+  --encoder_model_path /../model/haenkaze/tabnet-pretrain-out2023-40dim
--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@ export CUDA_VISIBLE_DEVICES=0,1
 
 python train.py \
   --dataset_name paired_data \
-  --datasets_dpath /mnt/iot-qnap5/miyauchi/data \
+  --datasets_dpath /../data \
   --rinna_gpt_name gpt_medium \
   --per_gpu_train_batch_size 128 \
   --per_gpu_eval_batch_size 128 \
@@ -14,4 +14,5 @@ python train.py \
   --num_layers 4 \
   --n_gpu 2 \
   --epochs 20 \
-  --only_prefix
+  --only_prefix \
+  --checkpoints_dpath /../model/haenkaze/prefix_tuning/checkpoints \
